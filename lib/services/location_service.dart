7ea50bf2f13bcb0538f6a054657a8e16537efbbc
Joe Kaldas <joekaldas8@gmail.com>
2022-06-06 00:17:13 +0200

    change location updates speed

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -16,7 +16,7 @@ class LocationService {
       yield currentLocation;
 
       // Change this to make navigation faster/slower
-      await Future.delayed(const Duration(milliseconds: 10));
+      await Future.delayed(const Duration(milliseconds: 200));
     }
   }
 
